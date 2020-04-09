1fdaf0fb2af4c73bb6f6db969b1414c733a74d2c
luversof <luversof@hanmail.net>
2020-04-10 06:00:43 +0900

    .

--- pages/blog/_id/write.vue
+++ pages/blog/_id/write.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>글쓰기</div>
+  <div>
+    글쓰기
+    <editor />
+    <b-button>{{ $t("blog.article.write") }}</b-button>
+  </div>
 </template>
 
 <script>
 import blogMixin from "@/assets/blog/blog.js";
 import blogArticleMixin from "@/assets/blog/blogArticle.js";
 
+import "codemirror/lib/codemirror.css";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/vue-editor";
+
 export default {
   mixins: [blogMixin, blogArticleMixin],
+  components: {
+    editor: Editor
+  },
   watch: {},
   // asyncData({ params }) {
   //   console.log(params);
