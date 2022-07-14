4b99e13a8ee7fb634f15ea692dffcf8ba38c0021
bluesky <luversof@hanmail.net>
2022-07-14 23:49:08 +0900

    layout, component 호출 테스트

---
+++ bluesky-project-front-sveltekit/src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Styles } from 'sveltestrap';
+	import BlueskyNavBar from '$lib/BlueskyNavBar.svelte';
+</script>
+
+<Styles />
+<BlueskyNavBar />
+
+<slot />
--- bluesky-project-front-sveltekit/src/routes/index.svelte
+++ bluesky-project-front-sveltekit/src/routes/index.svelte
@@ -1,2 +1,24 @@
+<script>
+	$: userInfo = fetch('/api/user/loginInfo', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		}
+	}).then((response) => response.json());
+
+	import { Button } from 'sveltestrap';
+</script>
+
+{#await userInfo}
+	<p>...Loading</p>
+{:then userInfo}
+	id : {userInfo.id}<br />
+	name : {userInfo.name}
+{:catch error}
+	<p>오류가 발생했습니다.</p>
+{/await}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button>test</Button>
