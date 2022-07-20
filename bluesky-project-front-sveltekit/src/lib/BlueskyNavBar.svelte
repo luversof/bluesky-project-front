021e7c0f7a0c1d7cd2fd505642d6601e3c034782
bluesky <luversof@hanmail.net>
2022-07-20 22:21:01 +0900

    navbar 정리

--- bluesky-project-front-sveltekit/src/lib/BlueskyNavBar.svelte
+++ bluesky-project-front-sveltekit/src/lib/BlueskyNavBar.svelte
@@ -8,17 +8,17 @@
 	}
 </script>
 
-<nav class="flex items-center justify-between flex-wrap bg-gray-800 p-2 fixed w-full z-10 top-0">
-	<div class="flex items-center flex-shrink-0 text-white mr-6">
-		<a class="text-white no-underline hover:text-white hover:no-underline" href="/">
+<nav class="flex items-center justify-between flex-wrap bg-white-800 fixed w-full z-10 top-0">
+	<div class="flex items-center flex-shrink-0 mr-6">
+		<a class="no-underline  hover:no-underline" href="/">
 			<span class="text-2xl pl-2">Bluesky</span>
 		</a>
 	</div>
 
-	<div class="block lg:hidden">
+	<div class="block lg:hidden p-2">
 		<button
 			on:click={menuToggle}
-			class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+			class="flex items-center px-3 py-2 border rounded text-gray-400 border-gray-400 hover:text-black hover:border-black"
 		>
 			<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
 				><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg
@@ -28,18 +28,18 @@
 
 	<div
 		class:hidden={isMenuShow}
-		class="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block pt-6 lg:pt-0"
+		class="w-full flex-grow lg:flex lg:items-center lg:w-auto lg:block pt-2 lg:pt-0 bg-white"
 	>
 		<ul class="list-reset lg:flex justify-start flex-1 items-center">
 			<li class="mr-3">
 				<a
-					class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+					class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 					href="/blog">Blog</a
 				>
 			</li>
 			<li class="mr-3">
 				<a
-					class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+					class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 					href="/bookkeeping">Bookkeeping</a
 				>
 			</li>
@@ -53,27 +53,27 @@
 			</ul>
 		{:else}
 			<ul class="list-reset flex justify-end flex-1 items-center">
-				<li class="mr-3">
+				<li>
 					<a
-						class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+						class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 						href="/oauth2/authorization/google"><i class="fa-brands fa-google" /></a
 					>
 				</li>
-				<li class="mr-3">
+				<li>
 					<a
-						class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+						class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 						href="/oauth2/authorization/facebook"><i class="fa-brands fa-facebook" /></a
 					>
 				</li>
-				<li class="mr-3">
+				<li>
 					<a
-						class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+						class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 						href="/oauth2/authorization/github"><i class="fa-brands fa-github" /></a
 					>
 				</li>
-				<li class="mr-3">
+				<li>
 					<a
-						class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
+						class="inline-block text-gray-400 no-underline hover:text-black hover:text-underline py-2 px-4"
 						href="/oauth2/authorization/battlenet"><i class="fa-brands fa-battle-net" /></a
 					>
 				</li>
